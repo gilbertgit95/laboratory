3aa0c530d77f4a4a2e1f3431f0d89cf95eb3a70c
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-21 10:17:11 +0800

    update

---
+++ python/intro/main.py
@@ -0,0 +1,11 @@
+# console input and output
+name = input('enter you name: ')
+print('Hello ' + name + '!')
+
+# data type conversion
+# int()
+# float()
+# bool()
+# str()
+age = input('enter you age: ')
+print('So you are born on the year ' + str(2022 - int(age)))
