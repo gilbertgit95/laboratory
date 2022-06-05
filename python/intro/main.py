74734d56b4d2ab2791cbaca586cbb48553b5988a
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-06-05 16:11:20 +0800

    update test

--- python/intro/main.py
+++ python/intro/main.py
@@ -16,11 +16,21 @@ load_dotenv()
 # str()
 # age = input('enter you age: ')
 # print('So you are born on the year ' + str(2022 - int(age)))
-hash = cryptH.btoa('test I')
-print(f'decrypt: { cryptH.atob(hash) }')
 
-hashedP = cryptH.hash_password('passtest')
-print(f'{ hashedP }: { cryptH.compare_text_to_hash("passtest", hashedP) }')
+
+# hash = cryptH.btoa('test I')
+# print(f'decrypt: { cryptH.atob(hash) }')
+
+# hashedP = cryptH.hash_password('passtest')
+# print(f'{ hashedP }: { cryptH.compare_text_to_hash("passtest", hashedP) }')
+
+
 
 fileH.list_files('list')
 
+
+# dictDataA = {'a': 1,'b': 3}
+# dictDataB = {'b': 4,'c': 2}
+# dictDataA['e'] = 5
+# dictDataA.update(dictDataB)
+# print(dictDataA)
