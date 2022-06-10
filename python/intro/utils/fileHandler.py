c07e6b3142be6be3211b03f9413bb94fa82375b8
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2022-06-10 20:36:09 +0800

    update filehandler

--- python/intro/utils/fileHandler.py
+++ python/intro/utils/fileHandler.py
@@ -2,6 +2,11 @@ def list_files(dir):
     print(dir)
     return []
 
-def createFile() {
-    print 'create a file here'
-}
+def createFile():
+    print('create a file here')
+
+def deleteFile():
+    print('delete file')
+
+def writeIntoFile():
+    print('write into file')
