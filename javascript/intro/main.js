564c04f8e1be5272ea5763cf3e9cb3a6c44ea47b
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2022-07-14 19:04:28 +0800

    update js intro

--- javascript/intro/main.js
+++ javascript/intro/main.js
@@ -1,4 +1,7 @@
-(() => {
-    console.log('proceess has started')
+const utils = require('./utils');
 
+(async () => {
+    console.log('proceess has started')
+    await utils.waitFor(2)
+    console.log('finish waiting.')
 })();
