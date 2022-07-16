a2da07f9c099b6dc306b1deef1ea4c86233d0e82
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-07-16 21:42:57 +0800

    init extract

--- python/excelExtract/main.py
+++ python/excelExtract/main.py
@@ -1,4 +1,5 @@
 import utils.fileHandler as fh
+import xlrd
 
 acceptableFiles = { 'xls', 'xlsx', 'csv' }
 
@@ -7,3 +8,13 @@ print(acceptableFiles.issubset('xls'))
 
 files = fh.listDirFiles('/excelFiles')
 print('files: ', files)
+
+# Give the location of the file
+loc = files[1]
+ 
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+ 
+# For row 0 and column 0
+print(sheet.cell_value(0, 0))
