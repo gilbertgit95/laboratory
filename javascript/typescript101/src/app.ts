4d76dce9ec55bce56aa2ed570a0852f062b7eed3
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-05-21 20:59:52 +0800

    basic express app

--- javascript/typescript101/src/app.ts
+++ javascript/typescript101/src/app.ts
@@ -1,9 +1,19 @@
+import express from 'express'
 import config, { Env } from './config'
 
-console.log('test')
+const env: Env = config.env
+const app = express()
+// console.log('test')
 
-for (let [key, val] of Object.entries(config.env)) {
-    console.log(`${ key }: ${ val }`)
-}
+// for (let [key, val] of Object.entries(config.env)) {
+//     console.log(`${ key }: ${ val }`)
+// }
 
-let env: Env = config.env
+
+app.get('/', (req, res) => {
+    return res.send('Hello!')
+})
+
+app.listen(env.DEFAULT_PORT, () => {
+    console.log(`Server is running on port: ${ env.DEFAULT_PORT }`)
+})
