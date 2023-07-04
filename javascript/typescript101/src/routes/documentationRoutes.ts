a88fec0e3bfffa60e7f2831ffd67f582be3061b7
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-07-05 00:58:46 +0800

    minor changes

--- javascript/typescript101/src/routes/documentationRoutes.ts
+++ javascript/typescript101/src/routes/documentationRoutes.ts
@@ -9,13 +9,14 @@ const router = express.Router()
 const env = config.getEnv()
 
 // update some swagger data
-// author, title, description, version, license, port
+// author, title, description, version, license, port, basePath
 swaggerData.info.title = appPackageInfo.name
 swaggerData.info.description = appPackageInfo.description
 swaggerData.info.version = appPackageInfo.version
 swaggerData.info.contact.email = appPackageInfo.author
 swaggerData.info.license.name = appPackageInfo.license
 swaggerData.host = `localhost:${ env.AppPort }`
+swaggerData.basePath = env.RootApiEndpoint
 
 router.use(env.RootApiEndpoint + 'apiDoc', swaggerUI.serve, swaggerUI.setup(swaggerData))
 
