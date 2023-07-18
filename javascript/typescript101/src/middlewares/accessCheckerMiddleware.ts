e8d90a3f87da3077ec29bd45e9972c1ee22333bd
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-07-18 15:15:32 +0800

    logger class and default loggers

--- javascript/typescript101/src/middlewares/accessCheckerMiddleware.ts
+++ javascript/typescript101/src/middlewares/accessCheckerMiddleware.ts
@@ -1,6 +1,10 @@
+import { errorLogsColl, combinedLogsColl } from '../utilities/logger'
+
 class AccessChecker {
     public async middleware(req:any, res:any, next:any) {
         console.log('AccessChecker')
+        combinedLogsColl.log({level: 'info', message: 'access checker'})
+        errorLogsColl.log({level: 'error', message: 'access checker'})
         next()
     }
 }
--- javascript/typescript101/src/utilities/logger.ts
+++ javascript/typescript101/src/utilities/logger.ts
@@ -1,17 +1,51 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
 
 import Config from '../config'
 
 const env = Config.getEnv()
+const { combine, timestamp, printf } = format
+
+interface ILogParams {
+    level: string,
+    message: string
+}
 
 class Logger {
-    constructor() {
-        console.log(env.AppLogsDirectory)
+    public logger
+    constructor(logCollection:string|null) {
+        this.logger = createLogger({
+            transports: [
+                new transports.Console(),
+                new DailyRotateFile({
+                    filename: (typeof logCollection === 'string')? `${ env.AppLogsDirectory }/${ logCollection }/%DATE%.log`: `${ env.AppLogsDirectory }/%DATE%.log`,
+                    datePattern: 'YYYY-MM-DD-HH',
+                    zippedArchive: true,
+                    maxSize: '20m',
+                    maxFiles: '365d'
+                })
+            ],
+            format: combine(
+                timestamp(),
+                printf(({ level, message, timestamp }) => {
+                    return `${ timestamp } [${ level }]: ${ message }`
+                })
+            )
+        })
     }
-    public async log() {
 
+    public log(obj:ILogParams) {
+        this.logger.log(obj)
     }
+}
+
+const errorLogsColl = new Logger('errorLogs')
+const combinedLogsColl = new Logger('combinedLogs')
+
+export {
+    ILogParams,
+    errorLogsColl,
+    combinedLogsColl
 }
 
 export default Logger
