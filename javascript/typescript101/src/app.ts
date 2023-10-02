b43df5d0a6449a3c24e1c4357d8271c102bac71c
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-10-02 22:03:45 +0800

    prepare for kagiweb-api-core-a testing

--- javascript/typescript101/src/app.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-
-import routes from './routes'
-
-export default express().use(routes)
--- javascript/typescript101/src/index.ts
+++ javascript/typescript101/src/index.ts
@@ -1,9 +1,14 @@
-import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
-import app from './app'
+import mongoose from 'mongoose'
+import express from 'express'
+import appRouteHandler from './app'
 import Config from './utilities/config'
 
 const env = Config.getEnv()
+const appRoutes = appRouteHandler.getAppRoutes()
+const app = express().use(appRoutes)
 
 app.listen(env.AppPort, async () => {
     try {
@@ -11,6 +16,8 @@ app.listen(env.AppPort, async () => {
             dbName: env.DBName
         })
         console.log(`- Successfully connected to database`)
+        await appRouteHandler.executePostDBConnectionProcess()
+        console.log(`- Execute post db conection process`)
     } catch (err) {
         console.log(`!Error, was not able to connect to the mongo database: ${ err }`)
         // throw(err)
