c333c4008bd2fa46068a1980031f2786544cfdb3
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-18 17:44:37 +0800

    update init structure

--- python/restApi/src/app.py
+++ python/restApi/src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
-print(__name__)
 
+# root endpoint
 @app.route('/')
 def rootRoute():
     return {
         'data': 'test'
     }
+
+# execute the server
+if __name__ == '__main__':
+    print(__name__)
+    app.run(debug=True)
