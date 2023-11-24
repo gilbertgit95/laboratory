d2279458ceffbe1a03b2ed40a135d3ecc2161199
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-24 13:24:01 +0800

    add refs to get requests data

--- python/restApi/src/routes/authRoutes.py
+++ python/restApi/src/routes/authRoutes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from controllers.authControllers import signinController, signoutController, signupController, forgotPasswordController, resetPasswordController
 
 authRoutes = Blueprint('authRoutes', __name__)
@@ -9,6 +9,29 @@ def signinRoute():
 
 @authRoutes.route('/signout', methods=['DELETE'])
 def signoutRoute():
+    paramData = {
+        'param1': request.args.get('param1'),
+        'param2': request.args.get('param2') 
+    }
+    bodyData = request.get_json()
+    formData = {
+        'form1': request.form.get('form1'),
+        'form2': request.form.get('form2')
+    }
+    formEncodedData = {
+        'sform1': request.form.get('sform1'),
+        'sform2': request.form.get('sform2')
+    }
+    headerData = {
+        'authorization': request.headers.get('authorization')
+    }
+
+    print(paramData)
+    print(bodyData)
+    print(formData)
+    print(formEncodedData)
+    print(headerData)
+
     return signoutController()
 
 @authRoutes.route('/signup', methods=['POST'])
