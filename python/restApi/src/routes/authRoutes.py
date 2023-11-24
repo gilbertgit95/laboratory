db7ebbee558333d4b2798391dd8e475dd642e4b1
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-25 01:15:39 +0800

    update for request data handling

--- python/restApi/src/routes/authRoutes.py
+++ python/restApi/src/routes/authRoutes.py
@@ -7,32 +7,36 @@ authRoutes = Blueprint('authRoutes', __name__)
 def signinRoute():
     return signinController()
 
-@authRoutes.route('/signout', methods=['DELETE'])
+@authRoutes.route('/signout', methods=['GET', 'DELETE'])
 def signoutRoute():
-    paramData = {
-        'param1': request.args.get('param1'),
-        'param2': request.args.get('param2') 
-    }
-    bodyData = request.get_json()
-    formData = {
-        'form1': request.form.get('form1'),
-        'form2': request.form.get('form2')
-    }
-    formEncodedData = {
-        'sform1': request.form.get('sform1'),
-        'sform2': request.form.get('sform2')
-    }
-    headerData = {
-        'authorization': request.headers.get('authorization')
-    }
 
-    print(paramData)
-    print(bodyData)
-    print(formData)
-    print(formEncodedData)
-    print(headerData)
+    if request.method == 'GET':
+        return { 'data': 'get data' }
+    elif request.method == 'DELETE':
+        paramData = {
+            'param1': request.args.get('param1'),
+            'param2': request.args.get('param2') 
+        }
+        bodyData = request.get_json()
+        formData = {
+            'form1': request.form.get('form1'),
+            'form2': request.form.get('form2')
+        }
+        formEncodedData = {
+            'sform1': request.form.get('sform1'),
+            'sform2': request.form.get('sform2')
+        }
+        headerData = {
+            'authorization': request.headers.get('authorization')
+        }
 
-    return signoutController()
+        print(paramData)
+        print(bodyData)
+        print(formData)
+        print(formEncodedData)
+        print(headerData)
+
+        return signoutController()
 
 @authRoutes.route('/signup', methods=['POST'])
 def signupRoute():
